dd763885a53814ecab0bc7ba39527bf36f221434
i3water <121024123@qq.com>
2018-09-26 12:06:20 +0800

    update codes, format codes

--- src/Adapters/BlinkerPRO.h
+++ src/Adapters/BlinkerPRO.h
@@ -323,25 +323,25 @@ class BlinkerPRO {
 
         bool authCheck() {
             uint8_t _authCheck;
-#ifdef BLINKER_DEBUG_ALL
-            BLINKER_LOG1(BLINKER_F("authCheck start"));
-#endif
+// #ifdef BLINKER_DEBUG_ALL
+//             BLINKER_LOG1(BLINKER_F("authCheck start"));
+// #endif
             EEPROM.begin(BLINKER_EEP_SIZE);
             EEPROM.get(BLINKER_EEP_ADDR_AUTH_CHECK, _authCheck);
             if (_authCheck == BLINKER_AUTH_CHECK_DATA) {
                 EEPROM.commit();
                 EEPROM.end();
                 isAuth = true;
-#ifdef BLINKER_DEBUG_ALL
-                BLINKER_LOG1(BLINKER_F("authCheck end"));
-#endif
+// #ifdef BLINKER_DEBUG_ALL
+//                 BLINKER_LOG1(BLINKER_F("authCheck end"));
+// #endif
                 return true;
             }
             EEPROM.commit();
             EEPROM.end();
-#ifdef BLINKER_DEBUG_ALL
-            BLINKER_LOG1(BLINKER_F("authCheck end"));
-#endif
+// #ifdef BLINKER_DEBUG_ALL
+//             BLINKER_LOG1(BLINKER_F("authCheck end"));
+// #endif
             return false;
         }
 
