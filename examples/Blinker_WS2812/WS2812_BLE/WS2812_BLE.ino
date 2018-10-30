c2c8bfcce94983761e1246750fcadcea42187523
i3water <121024123@qq.com>
2018-10-30 18:22:15 +0800

    update codes, upgrade WS2812 example

--- examples/Blinker_WS2812/WS2812_BLE/WS2812_BLE.ino
+++ examples/Blinker_WS2812/WS2812_BLE/WS2812_BLE.ino
@@ -58,7 +58,7 @@ void ws2812_callback(uint8_t r_value, uint8_t g_value, uint8_t b_value, uint8_t 
     uint8_t colorB = map(b_value, 0, 255, 0, bright_value);
 
     for(int i = 0; i < NUMPIXELS; i++){
-        pixels.setPixelColor(i, pixels.Color(colorR,colorG,colorB));
+        pixels.setPixelColor(i, colorR, colorG, colorB);
         pixels.show();
     }
 }
