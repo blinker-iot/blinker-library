9b2489b7db6afe352b72bb07071970ab2173c12a
i3water <121024123@qq.com>
2019-01-11 18:27:09 +0800

    update codes, upgrade BLINKER DATA codes

--- src/Blinker/BlinkerDataTicker.h
+++ src/Blinker/BlinkerDataTicker.h
@@ -1,16 +1,35 @@
-#ifndef BlinkerDataTicker_H
-#define BlinkerDataTicker_H
+// #ifndef BlinkerDataTicker_H
+// #define BlinkerDataTicker_H
 
-#if defined(ESP8266) || defined(ESP32)
+// #if defined(ESP8266) || defined(ESP32)
 
-#include "Blinker/BlinkerConfig.h"
-#include "Blinker/BlinkerDebug.h"
-#include "Blinker/BlinkerTimer.h"
+// #include "Blinker/BlinkerConfig.h"
+// #include "Blinker/BlinkerDebug.h"
+// #include "Blinker/BlinkerTimer.h"
 
-#include <Ticker.h>
+// #include <Ticker.h>
 
-Ticker dataTicker;
+// Ticker dataTicker;
 
-#endif
+// bool _dataTrigged = false;
+// uint32_t _data_time = 60;
 
-#endif
+// void dataInit(uint8_t mins, uint8_t seconds, uint32_t tickers = 60)
+// {
+//     _data_time = tickers;
+
+//     if (_data_time == 60)
+//     {
+//         uint32_t tTime = _data_time - seconds;
+//         dataTicker.
+//     }
+// }
+
+// void dataHandle()
+// {
+//     _dataTrigged = true;
+// }
+
+// #endif
+
+// #endif
