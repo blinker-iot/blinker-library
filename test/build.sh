9d0b66f190f22df72714568fa4ff15b7adcf0a6c
某10 <0w0@0w0.pw>
2019-07-31 01:00:39 +0800

    Add CI Matrix

--- test/build.sh
+++ test/build.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
+
+# 加,
+if [ -n "$LIB_URL" ]; then
+    export $TOOL_PATH=",$TOOL_PATH"
+fi
+
 # 生成构建配置
-echo -e "\e[93mGenerate esp8266.build.option.json\e[0m"
-cat <<EOF > $BUILD_HOME/esp8266.build.option.json
+echo -e "\e[93mGenerate build.option.json\e[0m"
+cat <<EOF > $BUILD_HOME/build.option.json
 {
   "additionalFiles": "",
   "builtInLibrariesFolders": "$ARDUINO_BUILDIN_LIB",
-  "customBuildProperties": "build.warn_data_percentage=75,runtime.tools.xtensa-lx106-elf-gcc.path=$ESP8266_PATH/esp8266/tools/xtensa-lx106-elf,runtime.tools.xtensa-lx106-elf-gcc-2.5.0-3-20ed2b9.path=$ESP8266_PATH/esp8266/tools/xtensa-lx106-elf,runtime.tools.mkspiffs.path=$ESP8266_PATH/esp8266/tools/mkspiffs,runtime.tools.mkspiffs-2.5.0-3-20ed2b9.path=$ESP8266_PATH/esp8266/tools/mkspiffs,runtime.tools.python.path=$ESP8266_PATH/esp8266/tools/python,runtime.tools.python-3.7.2-post1.path=$ESP8266_PATH/esp8266/tools/python",
-  "fqbn": "esp8266com:esp8266:wifiduino:xtal=80,vt=flash,exception=disabled,ssl=all,eesz=4M,ip=lm2f,dbg=Disabled,lvl=None____,wipe=none,baud=921600",
+  "customBuildProperties": "$BUILD_CONF",
+  "fqbn": "$BOARD_CONF",
   "hardwareFolders": "$ARDUINO_PATH/hardware,$BUILD_HOME/Arduino/hardware",
   "otherLibrariesFolders": "$BUILD_HOME/Arduino/libraries",
-  "toolsFolders": "$ARDUINO_PATH/tools-builder,$ARDUINO_PATH/hardware/tools/avr,$ESP8266_PATH/esp8266/tools"
+  "toolsFolders": "$ARDUINO_PATH/tools-builder,$ARDUINO_PATH/hardware/tools/avr$TOOL_PATH"
 }
 EOF
-echo -e "\e[93mDump esp8266.build.option.json:\e[92m"
-cat $BUILD_HOME/esp8266.build.option.json
-echo -e "\e[93mDone.\e[0m"
+cat $BUILD_HOME/build.option.json
+echo -e "\e[93mGenerated complete.\e[0m"
 
-echo -e "\e[42mStart Building.\e[0m"
+echo -e "\e[42mStart building...\e[0m"
 # 遍历ino文件
 for sketch in `find $TARGET_LIB -name '*.ino'`
     do
-        echo -e "\e[32mBuilding:$sketch\e[0m"
-        $ARDUINO_PATH/arduino-builder -build-options-file $BUILD_HOME/esp8266.build.option.json $sketch
-        echo -e "\e[32mDone.\e[0m"
+        echo -e "\e[32mBuilding:$sketch\e[93m"
+        $ARDUINO_PATH/arduino-builder -build-options-file $BUILD_HOME/build.option.json $sketch
     done
+echo -e "\e[32mBuild completed\e[0m"
--- test/install.sh
+++ test/install.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
+
 # 下载ArduinoIDE Linux64版本
 echo -e "\e[93mDownload ArduinoIDE...\e[0m"
 appveyor DownloadFile $ARDUINO_URL -FileName $BUILD_HOME/arduino-linux64.tar.xz
-# 下载ESP8266支持包
-echo -e "\e[93mDownload ESP8266 Package...\e[0m"
-appveyor DownloadFile $ESP8266_URL -FileName $BUILD_HOME/esp8266.zip
-# 解包
-echo -e "\e[93mUnpack ArduinoIDE...\e[0m"
+# 提取ArduinoIDE文件
+echo -e "\e[93mExtract ArduinoIDE...\e[0m"
 tar xf $BUILD_HOME/arduino-linux64.tar.xz -C $BUILD_HOME
-# 解包
-echo -e "\e[93mUnpack ESP8266 Package...\e[0m"
-unzip $BUILD_HOME/esp8266.zip -d $BUILD_HOME > /dev/null
-echo -e "\e[93mDone.\e[0m"
-# 安装ESP8266支持包
-echo -e "\e[93mInstall ESP8266 Library...\e[0m"
-mkdir -p $ESP8266_PATH
-# 复制目录
-cp -R $BUILD_HOME/$ESP8266_VER $ESP8266_PATH/esp8266
-cp -R $TARGET_LIB/test/package $ESP8266_PATH/esp8266/package
-cd $ESP8266_PATH/esp8266/tools
-python get.py
-echo -e "\e[93mDone.\e[0m"
-ls -la $ESP8266_PATH/esp8266/tools/python
+# 下载目标板支持包
+if [ -n "$LIB_URL" ]; then
+    echo -e "\e[93mDownload $TARGET...\e[0m"
+    appveyor DownloadFile $LIB_URL -FileName $BUILD_HOME/$TARGET.zip
+    # 提取板支持包
+    echo -e "\e[93mExtract $TARGET...\e[0m"
+    unzip $BUILD_HOME/$TARGET.zip -d $BUILD_HOME > /dev/null
+    echo -e "\e[93mInstall $TARGET...\e[0m"
+    mkdir -p $HWLIB_PATH
+    cp -R $BUILD_HOME/$LIB_VER $HWLIB_PATH
+    # 执行指定平台安装脚本
+    echo -e "\e[93mRun the $TARGET install script"
+    if [ "$TARGET" == "ESP8266" ]; then
+        # 从库中复制工具链索引文件
+        cp -R $TARGET_LIB/test/package $HWLIB_PATH/esp8266/package
+        cd $HWLIB_PATH/esp8266/tools
+        python get.py
+    fi
+else
+    echo -e "\e[93mNo package set, skip.\e[0m"
+fi
+echo -e "\e[93mInstall Done.\e[0m"
