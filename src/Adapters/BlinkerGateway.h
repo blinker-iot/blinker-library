1c64631a2802052b170655c3fa9cadcc7c5fb251
i3water <121024123@qq.com>
2019-08-03 10:15:17 +0800

    update codes, upgrade BLINKER PRINT codes

--- src/Adapters/BlinkerGateway.h
+++ src/Adapters/BlinkerGateway.h
@@ -846,61 +846,61 @@ int BlinkerGateway::print(char * data, bool needCheck)
         //     // payload += BLINKER_F("\",\"deviceType\":\"OwnApp\"}");
         // }
 
-        // uint16_t num = strlen(data);
+        uint16_t num = strlen(data);
 
-        // for(uint16_t c_num = num; c_num > 0; c_num--)
-        // {
-        //     data[c_num+7] = data[c_num-1];
-        // }
+        for(uint16_t c_num = num; c_num > 0; c_num--)
+        {
+            data[c_num+7] = data[c_num-1];
+        }
 
-        // data[num+8] = '\0';
-
-        // String data_add = BLINKER_F("{\"data\":");
-        
-        // for(uint16_t c_num = 0; c_num < 8; c_num++)
-        // {
-        //     data[c_num] = data_add[c_num];
-        // }
-
-        // data_add = BLINKER_F(",\"fromDevice\":\"");
-        // strcat(data, data_add.c_str());
-        // strcat(data, MQTT_DEVICEID_PRO);
-        // data_add = BLINKER_F("\",\"toDevice\":\"");
-        // strcat(data, data_add.c_str());
-        // if (_sharerFrom < BLINKER_MQTT_MAX_SHARERS_NUM)
-        // {
-        //     strcat(data, _sharers[_sharerFrom]->uuid());
-        // }
-        // else
-        // {
-        //     strcat(data, UUID_PRO);
-        // }
-        // data_add = BLINKER_F("\",\"deviceType\":\"OwnApp\"}");
-        // strcat(data, data_add.c_str());
-
-        // _sharerFrom = BLINKER_MQTT_FROM_AUTHER;
-
-        // if (!isJson(STRING_format(data))) return false;
+        data[num+8] = '\0';
 
         String data_add = BLINKER_F("{\"data\":");
-        data_add += data;
-        data_add += BLINKER_F(",\"fromDevice\":\"");
-        data_add += MQTT_DEVICEID_PRO;
-        data_add += BLINKER_F("\",\"toDevice\":\"");
+        
+        for(uint16_t c_num = 0; c_num < 8; c_num++)
+        {
+            data[c_num] = data_add[c_num];
+        }
+
+        data_add = BLINKER_F(",\"fromDevice\":\"");
+        strcat(data, data_add.c_str());
+        strcat(data, MQTT_DEVICEID_PRO);
+        data_add = BLINKER_F("\",\"toDevice\":\"");
+        strcat(data, data_add.c_str());
         if (_sharerFrom < BLINKER_MQTT_MAX_SHARERS_NUM)
         {
-            data_add += _sharers[_sharerFrom]->uuid();
+            strcat(data, _sharers[_sharerFrom]->uuid());
         }
         else
         {
-            data_add += UUID_PRO;
+            strcat(data, UUID_PRO);
         }
-
-        data_add += BLINKER_F("\",\"deviceType\":\"OwnApp\"}");
+        data_add = BLINKER_F("\",\"deviceType\":\"OwnApp\"}");
+        strcat(data, data_add.c_str());
 
         _sharerFrom = BLINKER_MQTT_FROM_AUTHER;
 
-        if (!isJson(data_add)) return false;
+        if (!isJson(STRING_format(data))) return false;
+
+        // String data_add = BLINKER_F("{\"data\":");
+        // data_add += data;
+        // data_add += BLINKER_F(",\"fromDevice\":\"");
+        // data_add += MQTT_DEVICEID_PRO;
+        // data_add += BLINKER_F("\",\"toDevice\":\"");
+        // if (_sharerFrom < BLINKER_MQTT_MAX_SHARERS_NUM)
+        // {
+        //     data_add += _sharers[_sharerFrom]->uuid();
+        // }
+        // else
+        // {
+        //     data_add += UUID_PRO;
+        // }
+
+        // data_add += BLINKER_F("\",\"deviceType\":\"OwnApp\"}");
+
+        // _sharerFrom = BLINKER_MQTT_FROM_AUTHER;
+
+        // if (!isJson(data_add)) return false;
         
         BLINKER_LOG_ALL(BLINKER_F("MQTT Publish..."));
         BLINKER_LOG_FreeHeap_ALL();
@@ -930,9 +930,9 @@ int BlinkerGateway::print(char * data, bool needCheck)
                 }
             }
 
-            if (! mqtt_PRO->publish(BLINKER_PUB_TOPIC_PRO, data_add.c_str()))
+            if (! mqtt_PRO->publish(BLINKER_PUB_TOPIC_PRO, data))
             {
-                BLINKER_LOG_ALL(data_add);
+                BLINKER_LOG_ALL(data);
                 BLINKER_LOG_ALL(BLINKER_F("...Failed"));
                 BLINKER_LOG_FreeHeap_ALL();
                 
@@ -944,7 +944,7 @@ int BlinkerGateway::print(char * data, bool needCheck)
             }
             else
             {
-                BLINKER_LOG_ALL(data_add);
+                BLINKER_LOG_ALL(data);
                 BLINKER_LOG_ALL(BLINKER_F("...OK!"));
                 BLINKER_LOG_FreeHeap_ALL();
                 
