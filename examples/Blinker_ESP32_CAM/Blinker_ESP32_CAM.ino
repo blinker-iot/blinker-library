4690ad7ce65190b429745e269c44bbbd86e6e3e2
i3water <121024123@qq.com>
2020-07-14 21:52:43 +0800

    update codes, upgrade BLINEKR ESP32 CAM support codes.

--- examples/Blinker_ESP32_CAM/Blinker_ESP32_CAM.ino
+++ examples/Blinker_ESP32_CAM/Blinker_ESP32_CAM.ino
@@ -44,51 +44,44 @@
  * *****************************************************************/
 
 #define BLINKER_WIFI
+#define BLINKER_ESP_SMARTCONFIG
 
 #include <Blinker.h>
+#include "ESP32_CAM_SERVER.h"
 
-char auth[] = "6650efab7f0a";
-char ssid[] = "有没有wifi";
-char pswd[] = "i8888888";
+char auth[] = "Your Device Secret Key";
+bool setup_camera = false;
 
-// 新建组件对象
-BlinkerButton Button1("btn-abc");
-BlinkerNumber Number1("num-abc");
-
-int counter = 0;
-
-// 按下按键即会执行该函数
-void button1_callback(const String & state)
-{
-    BLINKER_LOG("get button state: ", state);
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-}
-
-// 如果未绑定的组件被触发，则会执行其中内容
 void dataRead(const String & data)
 {
     BLINKER_LOG("Blinker readString: ", data);
-    counter++;
-    Number1.print(counter);
+
+    Blinker.vibrate();
+    
+    uint32_t BlinkerTime = millis();
+    
+    Blinker.print("millis", BlinkerTime);
 }
 
 void setup()
 {
-    // 初始化串口
     Serial.begin(115200);
     BLINKER_DEBUG.stream(Serial);
-    BLINKER_DEBUG.debugAll();
-    
-    // 初始化有LED的IO
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
-    // 初始化blinker
-    Blinker.begin(auth, ssid, pswd);
-    Blinker.attachData(dataRead);
 
-    Button1.attach(button1_callback);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+    
+    Blinker.begin(auth);
+    Blinker.attachData(dataRead);
 }
 
-void loop() {
+void loop()
+{
     Blinker.run();
+
+    if (Blinker.connected() && !setup_camera)
+    {
+        setupCamera();
+        setup_camera = true;
+    }
 }
