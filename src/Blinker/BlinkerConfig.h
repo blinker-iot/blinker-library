e9b7e15ede9648154cecfca19eb5178c57631438
i3water <121024123@qq.com>
2021-03-26 18:07:49 +0800

    update codes, upgrade BLINKER LIB to 0.3.7

--- src/Blinker/BlinkerConfig.h
+++ src/Blinker/BlinkerConfig.h
@@ -5,7 +5,7 @@
 // #include "Blinker/BlinkerUtility.h"
 #include "../Server/BlinkerServer.h"
 
-#define BLINKER_VERSION                 "0.3.6"
+#define BLINKER_VERSION                 "0.3.7"
 
 #define BLINKER_CONNECT_TIMEOUT_MS      10000UL
 
