cc1cd71d1b7d9030daee64539783485a0492b5fb
i3water <121024123@qq.com>
2022-01-23 22:53:38 +0800

    update codes, fix version codes error.

--- src/Blinker/BlinkerConfig.h
+++ src/Blinker/BlinkerConfig.h
@@ -5,7 +5,7 @@
 // #include "Blinker/BlinkerUtility.h"
 #include "../Server/BlinkerServer.h"
 
-#define BLINKER_VERSION                 "0.3.0.3.80211227"
+#define BLINKER_VERSION                 "0.3.80220123"
 
 #define BLINKER_CONNECT_TIMEOUT_MS      10000UL
 
